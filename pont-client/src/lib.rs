9ebec5ec830b6a3c2f78c0c27162baf5ace4a636
Matt Keeter <matt.j.keeter@gmail.com>
2020-04-08 19:12:19 -0400

    Things are talking over websockets!

--- pont-client/src/lib.rs
+++ pont-client/src/lib.rs
@@ -1,11 +1,27 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
+use std::sync::{Arc, Mutex};
+use web_sys::{WebSocket, MessageEvent};
+
+struct State {
+    last_message: String,
+    ws: WebSocket,
+}
+impl State {
+    fn on_message(&self, msg: JsValue) {
+    }
+
+    fn start(&self) {
+        self.ws.send_with_str("ping").expect("Failed to send ping");
+    }
+}
+
+type StateHandle = Arc<Mutex<State>>;
+
 // Called when the wasm module is instantiated
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
     let body = document.body().expect("document should have a body");
@@ -31,13 +47,29 @@ pub fn main() -> Result<(), JsValue> {
     svg.append_child(&circle)?;
     body.append_child(&svg)?;
 
-    let ws = web_sys::WebSocket::new("ws://0.0.0.0:8080")?;
-    let cloned_ws = ws.clone();
+    let ws = WebSocket::new("ws://0.0.0.0:8080")?;
+
+    let handle = Arc::new(Mutex::new(State {
+        last_message: String::new(),
+        ws: ws.clone()
+    }));
+
+    let handle_ = handle.clone();
     let onopen_callback = Closure::wrap(Box::new(move |_| {
-        val.set_inner_html(&format!("{:?}", cloned_ws.send_with_str("ping")));
-    }) as Box<dyn FnMut(JsValue)>);
+            let state = handle_.lock().unwrap();
+            state.start();
+        }) as Box<dyn FnMut(JsValue)>);
     ws.set_onopen(Some(onopen_callback.as_ref().unchecked_ref()));
     onopen_callback.forget();
+
+    let handle_ = handle.clone();
+    let onmessage_callback = Closure::wrap(Box::new(move |e: MessageEvent| {
+        let state = handle_.lock().unwrap();
+        let message = e.data();
+        state.on_message(message);
+    }) as Box<dyn FnMut(MessageEvent)>);
+    ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
+    onmessage_callback.forget();
 
     Ok(())
 }
