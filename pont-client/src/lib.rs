6647bd4350047f4de7e559b0c36f501559b9debc
Matt Keeter <matt.j.keeter@gmail.com>
2020-04-08 08:49:48 -0400

    First commit

---
+++ pont-client/src/lib.rs
@@ -0,0 +1,39 @@
+use wasm_bindgen::prelude::*;
+
+// Called when the wasm module is instantiated
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust!");
+
+    body.append_child(&val)?;
+
+    // Add an SVG
+    let svg = document.create_element_ns(Some("http://www.w3.org/2000/svg"), "svg")?;
+    svg.set_attribute("width", "100")?;
+    svg.set_attribute("hight", "100")?;
+    svg.set_attribute("viewBox", "0 0 100 100")?;
+
+    let circle = document.create_element_ns(Some("http://www.w3.org/2000/svg"), "circle")?;
+    circle.set_attribute("cx", "50")?;
+    circle.set_attribute("cy", "50")?;
+    circle.set_attribute("r", "20")?;
+    circle.set_attribute("stroke", "black")?;
+    circle.set_attribute("fill", "red")?;
+    svg.append_child(&circle)?;
+    body.append_child(&svg)?;
+
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn add(a: u32, b: u32) -> u32 {
+    a + b
+}
